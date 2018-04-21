ec1a39144cda5f1ebba8803c045fe37a4c4764ec
Avi <apilly@stevens.edu>
2018-04-21 16:06:28 -0400

    added comments and aslo made it so that the data is saved to a file

--- test.py
+++ test.py
@@ -12,20 +12,28 @@ data = r.json()
 
 final = {}
 
+#create dictionary
+# keys day
+# value stock values at the end of each day
 for item in data['Time Series (Daily)']:
 	datekey = datetime.strptime(item, '%Y-%m-%d')
 	final[datekey] = data['Time Series (Daily)'][item]['4. close']
 
+#sort dictionary based on keys
 od = collections.OrderedDict(sorted(final.items()))
 
+#list to store data
 lst = []
 
+#create a list with first column the date , second  column of 
 for k, v in od.items(): 
 	lst.append([k, v, 0.0])
 
+#convert stock values to percentage increases or decreases
 for i in range(1, len(lst)):
 	lst[i][-1] = 100.0*((float(lst[i][1]) - float(lst[i-1][1]))/float(lst[i-1][1]))
 
+# n defines how big we want to make our features
 n = 10
 vectors = []
 for i in range(n, len(lst)):
@@ -41,10 +49,49 @@ finalvectors = []
 for vector in vectors:
 	temp = []
 	for attribute in vector:
+		#grab stock percentage from each attribute
 		temp.append(attribute[-1])
 		print(str(attribute[0]) + ": " + str(attribute[-1]))
 	finalvectors.append(temp)
 	print("----------")
 
+#convert first columns as idenitfier of which class data belongs too
+#classes 
+#0 is less than -10 percent
+#1 is between -10 and -5 percent
+#2 is between -5 and -1 percent
+#3 is between -1 and 0 percent
+#4 is between 0 and 1 percent
+#5 is between 1 and 5 percent
+#6 is between 5 and 10  percent
+#7 is greater than 10 percent
+for v in finalvectors:
+	if (v[0]<-10):
+		v[0]=0
+	elif (v[0]<-5):
+		v[0]=1
+	elif (v[0]<-1):
+		v[0]=2
+	elif (v[0]<0):
+		v[0]=3
+	elif (v[0]<1):
+		v[0]=4
+	elif (v[0]<5):
+		v[0]=5
+	elif (v[0]<10):
+		v[0]=6
+	else:
+		v[0]=7
+
+f = open("data","w")
+for v in finalvectors:
+	s=str(v[0])	
+	for i in range(1,n):
+		s+=","		
+		s+=str(v[i])
+	s+="\n"
+	f.write(s)
+		
+f.close()
 for item in finalvectors:
 	print(item)
